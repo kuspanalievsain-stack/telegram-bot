684972de93d9481a73bf81d7b7ca625606242ea4
kuspanalievsain-stack <kuspanalievsain@gmail.com>
2026-07-24 22:58:59 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -267,6 +267,7 @@ def main():
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("clear", clear))
     application.add_handler(CommandHandler("newchat", newchat))
+       application.add_handler(CommandHandler("help", help_command))
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
     application.add_error_handler(error_handler)
     
